4f2baa17a91f7e9b90e9bb814a81f6655d444163
unknown <karadiyasadikali@gmail.com>
2021-04-08 00:04:39 +0530

    first commit

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+const mongoUrl = "mongodb+srv://sadikali:galkfa93PptFIZlt@cluster0.ofbva.mongodb.net/instagramDB?retryWrites=true&w=majority";
+
+//const mongoUrl = "mongodb://localhost:27017/instagramDB"
+const JWT_SECRET_KEY = "mynameiskaradiyasadikalishabbirali"
+const nodemailerAPI_KEY = "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"
+const EMAIL = 'http://localhost:3000'
+
+module.exports = {mongoUrl, JWT_SECRET_KEY, nodemailerAPI_KEY, EMAIL};
